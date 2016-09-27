dbb47416a9cdc5d0e8281a583dbf9d644bf354c3
Nick Riddell <nickjriddell@gmail.com>
2016-09-27 11:38:15 +0100

    browser history added

--- course-directory/src/router.js
+++ course-directory/src/router.js
@@ -1,6 +1,6 @@
-//Libs
+// Libs
 import React from 'react';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
 
 // Components
 import App from './components/App';
@@ -9,17 +9,16 @@ import About from './components/About';
 import Courses from './components/Courses';
 import Teachers from './components/Teachers';
 
-//Routes
-
-const routes = {
-    <Router history={hashHistory}>
-        <Route component={App}>
-            <Route path="/" component={Home} />
-            <Route path="about" component={About} />
-            <Route path="teachers" component={Teachers} />
-            <Route path="courses" component={Courses} />
-        </Route>
-    </Router>
-}
+// Routes
+const routes = (
+  <Router history={browserHistory}>
+    <Route component={App}>
+      <Route path="/" component={Home} />
+      <Route path="about" component={About} />
+      <Route path="teachers" component={Teachers} />
+      <Route path="courses" component={Courses} />
+    </Route>
+  </Router>
+);
 
 export default routes;
